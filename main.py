aa117f4f3c2f48b2a63abebdeb90bcdd37441e59
Dan Craciun <dancraciun@Dans-MacBook-Air.local>
2021-01-24 17:56:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+
+###### primul exercitiu
+# def my_function(a,b,c,d,e):
+#     if my_function() == 0:
+#         return 0
+#     if int(my_function()) == True:
+#         sum_my_function = sum(My_function())
+#
+#
+#
+# my_sum = my_function(1,5,-3,'abc', [12, 56, 'cad'])
+# print(sum_my_function)
+
+# def my_function():
+#     return 0
+#
+#
+# print(my_function)
+
+
+
+##### second exercise
+# def get_sum(n):
+#     if n == 0:
+#         return 0
+#
+#     return n + get_sum(n - 1)
+# print(get_sum(9))
+#
+# def sum_of_even(n):
+#     if n == 0:
+#         return 0
+#     if not n % 2 == 0:
+#         return sum_of_even(n-1)
+#     else:
+#         return n + sum_of_even(n-1)
+#
+# print(sum_of_even(4))
+
+# def sum_of_odd(n):
+#     if n == 0:
+#         return 0
+#     if not n % 2 != 0:
+#         return sum_of_even(n-1)
+#     else:
+#         return n + sum_of_even(n-1)
+#
+# print(sum_of_even(10))
+
+
+
+
+
